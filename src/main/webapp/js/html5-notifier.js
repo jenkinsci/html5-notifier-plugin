ecab7da729916e4c3140106f9d9736928e5b571e
Gavin Mogan <gavin@kodekoan.com>
2014-07-12 09:07:58 -0700

    add myself to the copyright of the .js file as I changed it quite a bit clean up pom file

--- src/main/webapp/js/html5-notifier.js
+++ src/main/webapp/js/html5-notifier.js
@@ -2,6 +2,7 @@
  * The MIT License
  * 
  * Copyright (c) 2011, Jesse Farinacci
+ * Copyright (c) 2014, Gavin Mogan
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
